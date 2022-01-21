436e4cf977d6dfdad795ea7dd85c4dc405b49d0a
rahicks26 <rahicks26@live.com>
2022-01-21 11:37:08 -0600

    Finished up exercise 2

--- unit-1/exercise-two/WinTail/WinTail.Console/ConsoleWriterActor.cs
+++ unit-1/exercise-two/WinTail/WinTail.Console/ConsoleWriterActor.cs
@@ -1,5 +1,6 @@
 using System;
 using Akka.Actor;
+using WinTail;
 
 namespace WinTail
 {
@@ -9,27 +10,26 @@ namespace WinTail
     /// </summary>
     internal class ConsoleWriterActor : UntypedActor
     {
+        // in ConsoleWriterActor.cs
         protected override void OnReceive(object message)
         {
-            var msg = message as string;
-
-            // make sure we got a message
-            if (string.IsNullOrEmpty(msg))
+            void onInputError(Messages.InputError msg)
             {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Please provide an input.\n");
-                Console.ResetColor();
-                return;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(msg.Reason);
             }
 
-            // if message has even # characters, display in red; else, green
-            var even = msg.Length % 2 == 0;
-            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
-            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
-            Console.ForegroundColor = color;
-            Console.WriteLine(alert);
-            Console.ResetColor();
+            void onSuccess(Messages.InputSuccess msg)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(msg.Reason);
+            }
 
+            if (message is Messages.InputError errorMsg) { onInputError(errorMsg); }
+            else if (message is Messages.InputSuccess successMsg) { onSuccess(successMsg); }
+            else { Console.WriteLine(message); }
+
+            Console.ResetColor();
         }
     }
 }
